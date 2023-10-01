a0d8a8dcd5a0b4762844d76c526ba362bef33889
AlLeonardBallesteros <142148707+AlLeonardBallesteros@users.noreply.github.com>
2023-10-01 19:47:35 +0800

    update ulit

--- src/components/pages/StudentCreateAccount.jsx
+++ src/components/pages/StudentCreateAccount.jsx
@@ -4,93 +4,110 @@ import axios from 'axios';
 import './pagescss/StudentCreateAccount.css';
 
 function StudentCreateAccountForm() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
+  const [firstname, setFirstName] = useState('');
+  const [lastname, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const navigate = useNavigate();
 
-    const handleCreateAccount = async () => {
-      if (firstName === '' || lastName === '' || email === '' || password === '') {
-        alert('All fields are required');
-      } else {
-        try {
-          const response = await axios.post('https://localhost:3000/api/signup', {
-            firstName,
-            lastName,
-            email,
-            password,
-          });
-  
-          if (response.status === 200) {
-            alert('Account created successfully');
-            navigate('/student-dashboard');
-          } else {
-            alert('Registration failed');
-          }
-        } catch (error) {
-          console.error('Error during registration:', error);
+  const handleCreateAccount = async () => {
+    if (firstname === '' || lastname === '' || email === '' || password === '') {
+      alert('All fields are required');
+    } else {
+      try {
+        const response = await axios.post('http://localhost:4000/user/signup', {
+          firstName,
+          lastName,
+          email,
+          password,
+        });
+
+        if (response.status === 200) {
+          alert('Account created successfully');
+          navigate('/student-login');
+        } else {
           alert('Registration failed');
         }
+      } catch (error) {
+        console.error('Error during registration:', error);
+        alert('Registration failed');
       }
-    };
+    }
+  };
 
   return (
     <div className="create-account-page">
-    <div className="create-account-box">
-      <h2>Create Account</h2>
-      <form>
-      <div className="input-group">
-        <label className="label" htmlFor="firstName">First Name:</label>
-          <input
-            name="firstName"
-            id="firstName"
-            type="text"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}/>
-            </div>
-      <div className="input-group">
-        <label className="label" htmlFor="lastName">Last Name:</label>
-          <input
-            name="lastName"
-            id="lastName"
-            type="text"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}/>
-            </div>
-      <div className="input-group">
-        <label className="label" htmlFor="email">Email:</label>
-          <input
-            name="email"
-            id="email"
-            type="email"
-            autoComplete="off"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}/>
-            </div>
-      <div className="input-group">
-        <label className="label" htmlFor="password">Password:</label>
-          <input
-            name="password"
-            id="password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}/>
-      </div>
-      <div className="button-group">
-        <button type="button"  onClick={handleCreateAccount} className="create-account-button">
-          SUMBIT
-        </button>
+      <div className="create-account-box">
+        <h2>Create Account</h2>
+        <form>
+          <div className="input-group">
+            <label className="label" htmlFor="firstName">
+              First Name:
+            </label>
+            <input
+              name="firstName"
+              id="firstName"
+              type="text"
+              value={firstname}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className="input-group">
+            <label className="label" htmlFor="lastName">
+              Last Name:
+            </label>
+            <input
+              name="lastName"
+              id="lastName"
+              type="text"
+              value={lastname}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <div className="input-group">
+            <label className="label" htmlFor="email">
+              Email:
+            </label>
+            <input
+              name="email"
+              id="email"
+              type="email"
+              autoComplete="off"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="input-group">
+            <label className="label" htmlFor="password">
+              Password:
+            </label>
+            <input
+              name="password"
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="button-group">
+            <button
+              type="button"
+              onClick={handleCreateAccount}
+              className="create-account-button"
+            >
+              SUBMIT
+            </button>
+          </div>
+        </form>
+        <div className="button-group">
+          <Link to="/student-login" className="login-button">
+            Log in
+          </Link>
         </div>
-      </form>
-      <div className="button-group">
-      <Link to="/student-login" className="login-button">Log in</Link>
       </div>
-    </div>
     </div>
   );
 }
-
 
 export default StudentCreateAccountForm;
